a68223f2bea11e55e1253f781e11cc5acb460c8c
Varun Arora <varuaror@codeaurora.org>
2026-01-14 19:38:41 +0800

    composer: Remove deprecated sync APIs
    APIs are marked as deprecated and using assert. Remove them.
    
    Change-Id: I92b170174f66eee6aafd8bcaabb8d9aa6d7f7c21

--- composer/hwc_buffer_sync_handler.cpp
+++ composer/hwc_buffer_sync_handler.cpp
@@ -108,16 +108,4 @@ void HWCBufferSyncHandler::GetSyncInfo(int fd, std::ostringstream *os) {
   }
 }
 
-DisplayError HWCBufferSyncHandler::SyncWait(int fd) {
-  // Deprecated.
-  assert(false);
-  return kErrorUndefined;
-}
-
-bool HWCBufferSyncHandler::IsSyncSignaled(int fd) {
-  // Deprecated.
-  assert(false);
-  return false;
-}
-
 }  // namespace sdm
